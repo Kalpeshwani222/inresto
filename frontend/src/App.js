ea1a87f3d92a88721846d69899d348d9d5d1f174
Kalpesh Wani <wanikalpeshanil@gmail.com>
2022-09-03 11:42:55 +0530

    separate out the components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,170 +1,19 @@
-import React, { useState, useEffect } from "react";
-import QRCode from "qrcode";
-import axios from "axios";
-import { saveAs } from "file-saver";
+import React from 'react'
+import { BrowserRouter, Route,Switch } from "react-router-dom";
+import AdminHome from './components/admin/AdminHome';
+import Home from './components/Home';
+import MenuItems from './components/user/MenuItems';
 
 const App = () => {
-  const [qrcode, setQrcode] = useState("");
-  const [table, setTables] = useState([]);
-
-  const GenerateQRCode = async (url, tableno) => {
-    QRCode.toDataURL(
-      url,
-      {
-        width: 800,
-        margin: 2,
-        color: {
-          dark: "#335383ff",
-        },
-      },
-      (err, url) => {
-        if (err) return console.log(err);
-        setQrcode(url);
-      }
-    );
-
-    //  saveAs(qrcode,`tableno-${tableno}`);
-  };
-
-  const downloadQRCode = () => {
-    saveAs(qrcode, `tableno`);
-  };
-
-  const getAllTables = async () => {
-    try {
-      const { data } = await axios.get(
-        "http://localhost:8000/api/table/get-all-tables"
-      );
-
-      setTables(data);
-    } catch (error) {
-      console.log(error.response.data.message);
-    }
-  };
-
-  useEffect(() => {
-    getAllTables();
-  }, []);
-
   return (
-    <>
-      {/* all tables */}
-      <table
-        style={{
-          width: "100%",
-          margin: "5rem",
-        }}
-      >
-        <tr>
-          {table.map((cur,ind) => {
-            return (
-              <>
-                {cur.status === "free" ? (
-                  <>
-                    <div
-                      style={{
-                        width: "200px",
-                        height: "130px",
-                        border: "1px solid",
-                        margin: "1px",
-                        display: "flex",
-                        justifyContent: "center",
-                        display: "inline-block",
-                        backgroundColor: "green",
-                      }}
-                    >
-                      <h1
-                        style={{
-                          marginRight: "auto",
-                          marginLeft: "auto",
-                          alignItems: "center",
-                          textAlign: "center",
-                        }}
-                      >
-                        {cur.tableno}
-                      </h1>
-                      <p>{cur.status}</p>
+    <BrowserRouter>
+      <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/admin/home" component={AdminHome} exact />
+           <Route path="/menu-items/:tableno" component={() => <MenuItems />} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
 
-                      <button
-                        style={{}}
-                        onClick={() =>
-                          GenerateQRCode(
-                            `http://localhost:3000/menu/${cur.tableno}`,
-                            cur.tableno
-                          )
-                        }
-                      >
-                        generate
-                      </button>
-                    </div>
-                  </>
-                ) : (
-                  <>
-                    <div
-                      style={{
-                        width: "200px",
-                        height: "130px",
-                        border: "1px solid",
-                        margin: "1px",
-                        display: "flex",
-                        justifyContent: "center",
-                        display: "inline-block",
-                        backgroundColor: "red",
-                      }}
-                    >
-                      <h1
-                        style={{
-                          marginRight: "auto",
-                          marginLeft: "auto",
-                          alignItems: "center",
-                          textAlign: "center",
-                        }}
-                      >
-                        {cur.tableno}
-                      </h1>
-                      <p>{cur.status}</p>
-
-                      <button
-                        style={{}}
-                        onClick={() =>
-                          GenerateQRCode(
-                            `http://localhost:3000/menu/${cur.tableno}`,
-                            cur.tableno
-                          )
-                        }
-                      >
-                        generate
-                      </button>
-                    </div>
-                  </>
-                )}
-              </>
-            );
-          })}
-        </tr>
-      </table>
-
-      <div className="qrCodeimg">
-        {qrcode && (
-          <>
-            <img
-              src={qrcode}
-              style={{
-                width: "200px",
-                height: "200px",
-                display: "block",
-                alignItems: "center",
-              }}
-            />
-
-            <button type="submit" onClick={() => downloadQRCode()}>
-              Download
-            </button>
-          </>
-        )}
-      </div>
-    </>
-  );
-};
-
-export default App;
+export default App
---
+++ frontend/src/components/user/MenuItems.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MenuItems = () => {
+  return (
+   <>
+       <h1>MenuItems</h1>
+   </>
+  )
+}
+
+export default MenuItems
